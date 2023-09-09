03f55c2e03a3c56f667493511ad92a0b78c8f419
Jorge Gomes <jorge.gomes@vost.pt>
2023-09-09 10:52:00 +0100

    Create morroco_keywords.py
    This commit creates lists with commonly used words in Morroco for the specified categories.

---
+++ morroco_keywords.py
@@ -0,0 +1,43 @@
+emergency_services_requests = [
+    ("تحتاج إلى إسعاف", "Besoin d'une ambulance", "Need an ambulance"),
+    ("حريق! بحاجة إلى إطفائيين", "Incendie ! Besoin de pompiers", "Fire! Need firefighters"),
+    ("تحتاج إلى شرطة", "Besoin de la police", "Need the police"),
+    ("هنا حادث, بحاجة للإسعاف", "Accident ici, besoin d'une ambulance", "Accident here, need an ambulance"),
+    ("كنت تعاني من نوبة صداع, أرجو المساعدة", "Je souffre d'une migraine, s'il vous plaît aidez-moi", "I'm suffering from a headache, please help"),
+    ("أشعر بضيق في التنفس", "Je ressens une difficulté à respirer", "I'm experiencing difficulty breathing"),
+    ("تحتاج إلى خدمات البحث والإنقاذ", "Besoin des services de recherche et de sauvetage", "Need search and rescue services"),
+    ("واجهت حالة طارئة, أحتاج المساعدة", "J'ai une situation d'urgence, j'ai besoin d'aide", "I have an emergency, I need assistance")
+]
+
+roads = [
+    ("الطريق مغلق", "Route bloquée", "Road blocked"),
+    ("جسر مهدم", "Pont effondré", "Collapsed bridge"),
+    ("الطريق مغمور بالمياه", "Route inondée", "Flooded road"),
+    ("انهيارات تربية", "Glissements de terrain", "Landslides"),
+    ("حفر عميقة", "Nids-de-poule", "Deep potholes"),
+    ("أضواء إشارة معطلة", "Feux de signalisation hors service", "Malfunctioning traffic lights"),
+    ("حادث سير", "Accident de la route", "Car accident"),
+    ("نفق مسدود", "Tunnel obstrué", "Blocked tunnel")
+]
+
+critical_infrastructure = [
+    ("مستشفى متضرر", "Hôpital endommagé", "Damaged hospital"),
+    ("مدرسة متهدمة", "École effondrée", "Collapsed school"),
+    ("مبنى مدمر", "Bâtiment en ruine", "Ruined building"),
+    ("انقطاع الكهرباء", "Électricité coupée", "Power outage"),
+    ("تلوث مياه الشرب", "Eau potable contaminée", "Contaminated drinking water"),
+    ("تسرب الغاز", "Gaz fuyant", "Gas leak"),
+    ("جسر مهدم", "Pont effondré", "Collapsed bridge"),
+    ("مطار مغلق", "Aéroport fermé", "Closed airport")
+]
+
+requests_for_help = [
+    ("عاونوني", "Aidez-moi", "Help me"),
+    ("من فضلك", "S'il vous plaît", "Please"),
+    ("ضروري", "Urgent", "Urgent"),
+    ("بغيت مساعدة", "Besoin d'aide", "Need help"),
+    ("النجدة", "Secours", "Emergency"),
+    ("SOS", "SOS", "SOS"),
+    ("مساعدة", "Assistance", "Assistance"),
+    ("تعاون", "Aide", "Assistance")
+]
